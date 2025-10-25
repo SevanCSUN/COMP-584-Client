2933782b2e7b1f8fe403760ba046320b4c78983a
SevanCSUN <sevan.shahijanian.112@my.csun.edu>
2025-10-24 17:45:18 -0700

    class work country population clicking on id to show pop and display link back

--- new-client/src/app/country-population/country-population.ts
+++ new-client/src/app/country-population/country-population.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopulationData } from './population-data';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
+import { ActivatedRoute } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.css'
 })
-export class CountryPopulation {
-  country!: PopulationData;
-  constructor(http: HttpClient) {
-    http.get<PopulationData>(environment.apiUrl + "api/Countries/population/").subscribe(result => {
-      this.country = result;
-    })
+
+export class CountryPopulation implements OnInit {
+  CountryPopulation!: PopulationData;
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+    
+  }
+
+  ngOnInit(): void {
+    let idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get<PopulationData>(`${environment.apiUrl}api/Countries/population/${idParam}`).subscribe(result => {
+      this.CountryPopulation = result;
+    });
   }
 }
